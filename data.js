c85358858bf4dc58e7f8a020873c3b8c97d0e264
Chris Kelly <chris@workinggears.com>
2021-10-10 02:04:43 +1100

    Add interfaceingame.com and gameuidatabase.com to data.js

--- data.js
+++ data.js
@@ -2,6 +2,7 @@
 Please use fields "Title", "Url", "Tags", and optional "Blurb".
 Existing tags: 
   "Pixel Art"
+  "UI"
   (Add more here)
 */
 
@@ -10,6 +11,18 @@ module.exports = [
     Title: "Miniboss Pixel art tutorials",
     Url: "https://blog.studiominiboss.com/pixelart",
     Tags: ["Pixel Art"]
+  },
+  {
+    Title: "Interface in Game",
+    Blurb: "An extensive collection of interfaces across various games and genres",
+    Url: "https://interfaceingame.com",
+    Tags: ["UI"]
+  },
+  {
+    Title: "Game UI Database",
+    Blurb: "An extensive collection of interfaces across various games and genres",
+    Url: "https://www.gameuidatabase.com",
+    Tags: ["UI"]
   },
   //Add more entries above this line ^
 ]
