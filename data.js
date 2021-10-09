7343358049f755abcf14a9d2e2e58c0cbbe46f12
Chris K <55621012+chrisk-7777@users.noreply.github.com>
2021-10-10 01:56:13 +1100

    Add Kenney Assets to data.js

--- data.js
+++ data.js
@@ -2,6 +2,7 @@
 Please use fields "Title", "Url", "Tags", and optional "Blurb".
 Existing tags: 
   "Pixel Art"
+  "Assets"
   (Add more here)
 */
 
@@ -11,5 +12,11 @@ module.exports = [
     Url: "https://blog.studiominiboss.com/pixelart",
     Tags: ["Pixel Art"]
   },
+  {
+    Title: "Kenney Assets",
+    Blurb: "Kenney has created thousands of sprites, 3D models and sound effects which you can use in your projects. The generous public domain license allows any kind of use, even commercial!",
+    Url: "https://www.kenney.nl/assets",
+    Tags: ["Assets"]
+  }
   //Add more entries above this line ^
 ]
