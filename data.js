858d72c287b9451ba7808031e96d9fd26ef7a0b7
Drew Conley <drewconley@gmail.com>
2021-10-09 08:46:20 -0600

    first data commit

---
+++ data.js
@@ -0,0 +1,15 @@
+/*
+Please use fields "Title", "Url", "Tags", and optional "Blurb".
+Existing tags: 
+  "Pixel Art"
+  (Add more here)
+*/
+
+module.exports = [
+  {
+    Title: "Miniboss Pixel art tutorials",
+    Url: "https://blog.studiominiboss.com/pixelart",
+    Tags: ["Pixel Art"]
+  },
+  //Add more entries above this line ^
+]
